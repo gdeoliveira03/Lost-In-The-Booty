172e292e5ebd025f9d9ba1745be08815f10ee33c
fundyp <144487692+fundyp@users.noreply.github.com>
2023-10-10 22:49:21 -0400

    Changed colors of trees
    changed colors on trees, functions in player controller script added but they are not active.

--- Lost in the Booty/Assets/Scripts/PlayerController.cs
+++ Lost in the Booty/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,31 @@ public class PlayerController1 : MonoBehaviour
     public float speed;
     public float walkSpeed = 4;
     public float runSpeed = 8;
-    private Vector2 move;
+    private Vector2 move, mouseLook;
     private float isRun = 0;
+    private float isAbility = 0;
 
     // send info to animator
     private Animator animator;
 
+    // accessed when shift is clicked
     public void onRun(InputAction.CallbackContext context) {
         isRun = context.ReadValue<float>();
     }
 
+    // called when ability is clicked
+    public void onAbility(InputAction.CallbackContext context) {
+        isAbility = context.ReadValue<float>();
+    }
+
+
+    // will be called for mouse vector 
+    public void onMouseLook(InputAction.CallbackContext context) {
+
+        mouseLook = context.ReadValue<Vector2>();
+    }
+
+    // accessed when "WASD" is pressed 
     public void OnMove(InputAction.CallbackContext context) {
         move = context.ReadValue<Vector2>();
     }
@@ -34,7 +49,10 @@ public class PlayerController1 : MonoBehaviour
     {
         movePlayer();
 
-        
+        // if the ability is pressed call ability function
+        if(isAbility != 0) {
+            useAbility();
+        }
     }
 
     public void movePlayer() {
@@ -47,8 +65,11 @@ public class PlayerController1 : MonoBehaviour
         // take in the movement
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        // character rotates and look at the location faced
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        if(movement != Vector3.zero) {
+            // character rotates and look at the location faced
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
 
         // have character move towards that location
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
@@ -56,5 +77,9 @@ public class PlayerController1 : MonoBehaviour
         // float to use as Speed parameter for animator
         float charSpeed = (movement.magnitude * speed);
         animator.SetFloat("Speed", charSpeed);
+    }
+
+    public void useAbility() {
+       
     }
 }
