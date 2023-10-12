83a0e080ea6cae027b012f5f012e424ee26dc152
fundyp <144487692+fundyp@users.noreply.github.com>
2023-10-12 00:17:52 -0400

    Added working ability buttons
    keyboard buttons 1 2 3 4 and 5 have been added to input manager as a temporary ability use. activation of these keys will stop player movement but will not invoke animation yet.

--- Lost in the Booty/Assets/Scripts/PlayerController.cs
+++ Lost in the Booty/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController1 : MonoBehaviour
 {
-    
+    private InputActionAsset InputAction;
+
     public float speed;
     public float walkSpeed = 4;
     public float runSpeed = 8;
@@ -19,13 +20,33 @@ public class PlayerController1 : MonoBehaviour
     // accessed when shift is clicked
     public void onRun(InputAction.CallbackContext context) {
         isRun = context.ReadValue<float>();
+        isAbility = 0; // allows player run again
     }
 
-    // called when ability is clicked
-    public void onAbility(InputAction.CallbackContext context) {
-        isAbility = context.ReadValue<float>();
+    // called when ability 1 is clicked
+    public void onAbility1(InputAction.CallbackContext context) {
+        isAbility = 1;
     }
 
+    // called when ability 2 is clicked
+    public void onAbility2(InputAction.CallbackContext context) {
+        isAbility = 2;
+    }
+
+    // called when ability 3 is clicked
+    public void onAbility3(InputAction.CallbackContext context) {
+        isAbility = 3;
+    }
+
+    // called when ability 4 is clicked
+    public void onAbility4(InputAction.CallbackContext context) {
+        isAbility = 4;
+    }
+
+    // called when ability 5 is clicked
+    public void onAbility5(InputAction.CallbackContext context) {
+        isAbility = 5;
+    }
 
     // will be called for mouse vector 
     public void onMouseLook(InputAction.CallbackContext context) {
@@ -47,12 +68,17 @@ public class PlayerController1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
-
+       
         // if the ability is pressed call ability function
         if(isAbility != 0) {
+
             useAbility();
+            movePlayer();
         }
+        else {
+            movePlayer();
+        }
+        
     }
 
     public void movePlayer() {
@@ -65,21 +91,28 @@ public class PlayerController1 : MonoBehaviour
         // take in the movement
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
+        if(isAbility == 0) 
+        {
+            if(movement != Vector3.zero) 
+            {
+                // character rotates and look at the location faced
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            }
 
-        if(movement != Vector3.zero) {
-            // character rotates and look at the location faced
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            // have character move towards that location
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
         }
-
-        // have character move towards that location
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        
 
         // float to use as Speed parameter for animator
         float charSpeed = (movement.magnitude * speed);
         animator.SetFloat("Speed", charSpeed);
     }
 
+    
     public void useAbility() {
-       
-    }
+        
+        Debug.Log("Ability used float is: " + isAbility);
+        
+    } 
 }
