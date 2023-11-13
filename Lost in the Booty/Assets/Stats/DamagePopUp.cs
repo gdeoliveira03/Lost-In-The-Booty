f29ce7d11a7acad53eb3758f85ba08dcfccea144
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-11-12 23:57:50 -0500

    Add files via upload

---
+++ Lost in the Booty/Assets/Stats/DamagePopUp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DamagePopUp : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float lifetime = 0.6f;
+    public float minDist = 2f;
+    public float maxDist = 3f;
+
+    private Vector3 iniPos;
+    private Vector3 targetPos;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(2 * transform.position - Camera.main.transform.position);
+
+        float direction = Random.rotation.eulerAngles.z;
+        iniPos = transform.position;
+        float dist = Random.Range(minDist, maxDist);
+        targetPos = iniPos + (Quaternion.Euler(0, 0, direction) * new Vector3(dist, dist, 0f));
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        float fraction = lifetime / 2f;
+
+        if (timer > lifetime) Destroy(gameObject);
+        else if (timer > fraction) text.color = Color.Lerp(text.color, Color.clear, (timer - fraction) / (lifetime - fraction));
+
+        transform.position = Vector3.Lerp(iniPos, targetPos, Mathf.Sin(timer / lifetime));
+        transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, Mathf.Sin(timer / lifetime));
+    }
+
+    public void SetDamageText(int damage)
+    {
+        text.text = damage.ToString();
+    }
+}
