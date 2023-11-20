367ebd2e700e558822527e3ed99f31c0ccfeed37
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-11-19 22:56:27 -0500

    Add files via upload

---
+++ Lost in the Booty/Assets/Stats/EnemyStats.cs
@@ -0,0 +1,230 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour
+{
+    private int damage = 0;
+    private int armor = 0;
+    private int evasion = 0;
+    private int attackspeed = 0;
+    private int movementspeed;
+    private int luck = 0;
+    private int healthregen = 0;
+    private int manaregen = 0;
+    
+    public int MaxHealth;
+    public int CurrentHealth {get; private set; }
+
+    public int MaxMana;
+    public int CurrentMana {get; private set; }
+
+    public GameObject DeadEnemy;
+    public GameObject DamageText;
+    public string EnemyType;
+
+    [SerializeField] FloatingHealthBar healthbar;
+
+    private bool isStunned = false;
+    public GameObject OnFireEffect;
+    public GameObject LightningElement;
+    public GameObject LightningExplosion;
+    public GameObject SlowEffect;
+    public GameObject FrostBiteEffect;
+    public GameObject FreezeEffect;
+    int originalMovementSpeed; 
+    private float slowtime = 0f;
+    private bool isslowed = false;
+
+    public string playerTag = "Player";
+
+
+    void Start()
+    {
+        // Set Enemy Stats Here
+        if (EnemyType == "Skeleton"){
+            damage = 5;
+            MaxHealth = 30;
+            CurrentHealth = MaxHealth;
+            MaxMana = 0;
+            armor = 0;
+            evasion = 0;
+            attackspeed = 0;
+            healthregen = 0;
+            manaregen = 0;
+            originalMovementSpeed = 100;
+            movementspeed = 100;
+
+        }
+
+        CurrentHealth = MaxHealth;
+        healthbar = GetComponentInChildren<FloatingHealthBar>();
+
+    }
+
+
+    void Update()
+    {
+        if(isslowed){
+            slowtime += Time.deltaTime;
+
+            if (slowtime >= slowDuration)
+            {
+                slowStacks = 0;
+                SlowEffect.SetActive(false);
+                isslowed = false;
+                slowtime = 0f;
+                movementspeed = originalMovementSpeed;
+            }
+        }
+        if (isStunned)
+        {
+            // Player cannot move
+        }
+
+    }
+
+    //STUNNED ENEMY
+
+    public void Stun()
+    {
+        isStunned = true;
+    }
+
+    IEnumerator RecoverFromStun()
+    {
+        yield return new WaitForSeconds(2f);
+
+        // Reset the flag and allow the enemy to move again
+        isStunned = false;
+    }
+
+    // Keeps Track of Burning   
+    private bool isburning = false;
+    private int activeDOTEffects = 0;
+
+    public IEnumerator TakeDamageOverTime(string DOTType, int damages, float duration, float tickSpeed)
+    {
+        float elapsedTime = 0f;
+
+        activeDOTEffects++;
+
+        while (elapsedTime < duration)
+        {
+            if(DOTType == "fire" && !isburning){
+                OnFireEffect.SetActive(true);
+                isburning = true;
+            }
+            if(DOTType == "ice"){
+                FrostBiteEffect.SetActive(true);
+            }
+
+            yield return new WaitForSeconds(tickSpeed);
+
+            // Apply damage
+            TakeDamage(damages);
+
+            // Update elapsed time
+            elapsedTime += tickSpeed;
+        }
+
+        // Decrement active DOT effects
+        activeDOTEffects--;
+
+        if (DOTType == "fire" && activeDOTEffects == 0)
+        {
+            OnFireEffect.SetActive(false);
+            isburning = false;
+        }
+        if (DOTType == "ice")
+        {
+            FrostBiteEffect.SetActive(false);
+        }
+    }
+
+    private int shockStacks = 0;
+    private int shockThreshold = 3;
+    private int shockDamage = 5; // Adjust the damage as needed
+
+    public void InflictShock()
+    {
+        shockStacks++;
+        LightningElement.SetActive(true);
+        
+        // Check if the shock threshold is reached
+        if (shockStacks >= shockThreshold)
+        {
+            // Reset shock stacks
+            shockStacks = 0;
+            LightningElement.SetActive(false);
+            LightningExplosion.SetActive(true);
+            Invoke("StopLightningExplosion", 0.5f);
+
+            // Apply shock damage
+            TakeDamage(shockDamage);
+        }
+    }
+
+    public void StopLightningExplosion (){
+        LightningExplosion.SetActive(false);
+    }
+
+
+
+
+
+    private int slowStacks = 0;
+    private int maxSlowStacks = 4;
+    private float slowDuration = 0f;
+   
+
+    public void ApplySlow(int stacksToAdd)
+    {
+        slowStacks = Mathf.Min(slowStacks + stacksToAdd, maxSlowStacks);
+        slowDuration = Mathf.Min(slowDuration + (stacksToAdd*5), 20f);
+        isslowed = true;
+
+        if (stacksToAdd > 0 && slowStacks == stacksToAdd)
+        {
+            SlowEffect.SetActive(true);
+        }
+
+        float newMovementSpeed = originalMovementSpeed * (1f - 0.2f * slowStacks);
+        movementspeed = Mathf.RoundToInt(newMovementSpeed);
+    }
+
+
+    public void freezeOn(){
+        FreezeEffect.SetActive(true);
+        movementspeed = 0;
+        Invoke("freezeOff", 5f);
+
+    }
+
+    public void freezeOff(){
+        FreezeEffect.SetActive(false);
+        movementspeed = originalMovementSpeed;
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damage -= armor;
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        CurrentHealth -= damage;
+        healthbar.UpdateHealthBar(CurrentHealth, MaxHealth);
+        DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+        indicator.SetDamageTextColor(Color.black);
+        indicator.SetDamageText(damage.ToString());
+
+        if (CurrentHealth <= 0){
+            Die();
+        }
+    }
+
+    public virtual void Die ()
+    {
+        //Destroy(DeadEnemy);
+    }
+}
---
+++ Lost in the Booty/Assets/Stats/ScruffyStats.cs
@@ -0,0 +1,340 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScruffyStats : MonoBehaviour
+{
+        public int damage = 5;
+        public int armor = 0;
+        public int evasion = 0;
+        private int movementspeed = 0;
+        private int luck = 0;
+
+        public int MaxHealth;
+        public int CurrentHealth {get; private set; }
+        public Slider healthBar;
+
+        public int MaxMana;
+        public int CurrentMana {get; private set; }
+        public Slider manaBar;
+
+        public GameObject DamageText;
+        public GameObject HealthText;
+
+        private float healthrestoreTimer = 0f;
+        private float manarestoreTimer = 0f;
+        private float manaRestoreInterval = 5f; // Restore mana every x seconds
+        private float healthRestoreInterval = 8f; // Restore health every y seconds
+
+        private bool isTempHealth = false;
+        private int TemporaryHealth;
+        SkillList skills;
+
+        private PlayerStateMachine MovementScript;
+        private float normalwalkSpeed;
+        private float normalrunSpeed;
+     
+        void Start ()
+        {
+            MovementScript = GetComponent<PlayerStateMachine>();
+            skills = GetComponent<SkillList>();
+
+            CurrentHealth = MaxHealth;
+            CurrentMana = MaxMana;
+            healthBar.maxValue = MaxHealth;
+            manaBar.maxValue = MaxMana;
+
+            normalwalkSpeed = MovementScript.walkSpeed;
+            normalrunSpeed = MovementScript.runSpeed;
+
+            damage = 5;
+        }
+
+
+        void Update (){
+
+            healthBar.value = CurrentHealth;
+            manaBar.value = CurrentMana;
+
+            // Natural Mana/Health regen
+            healthrestoreTimer += Time.deltaTime;
+            if (healthrestoreTimer >= healthRestoreInterval)
+            {
+                NaturalRestoreHealth(1);
+                healthrestoreTimer = 0f;
+            }
+
+            manarestoreTimer += Time.deltaTime;
+            if (manarestoreTimer >= manaRestoreInterval)
+            {
+                NaturalRestoreMana(1);
+                manarestoreTimer = 0f;
+            }
+
+            if (CurrentHealth <= 0){
+                Die();
+            }
+
+            
+            if (Input.GetKeyDown(KeyCode.T)){
+                TakeDamage(5);
+            }
+            if (Input.GetKeyDown(KeyCode.Y)){
+                RestoreMana(20);
+            }
+
+        
+        }
+
+        void NaturalRestoreHealth(int amount)
+        {
+            if (CurrentHealth + amount <= MaxHealth)
+            {
+                CurrentHealth += amount;
+                healthBar.value = CurrentHealth;
+            }
+        }
+
+        void NaturalRestoreMana(int amount)
+        {
+            if (CurrentMana + amount <= MaxMana)
+            {
+                CurrentMana += amount;
+                manaBar.value = CurrentMana;
+            }
+        }
+
+        public void RestoreMana(int amount){
+            if (CurrentMana + amount <= MaxMana)
+            {
+                CurrentMana += amount;
+                manaBar.value = CurrentMana;
+                DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                indicator.SetDamageText(amount.ToString());
+                indicator.SetDamageTextColor(Color.blue);
+            }
+            else
+            {
+                DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                indicator.SetDamageText((MaxMana-CurrentMana).ToString());
+                indicator.SetDamageTextColor(Color.blue);
+                CurrentMana = MaxMana;
+                manaBar.value = CurrentMana;
+            }
+        }
+
+
+        public void IncreaseArmor(int armorvalue){
+            armor += armorvalue;
+        }
+
+        public void IncreaseArmorEnd(int armorvalue){
+            armor -= armorvalue;
+        }
+
+        public void TempMaxHPIncrease(int value){
+            isTempHealth = true;
+            TemporaryHealth = value;
+        }
+
+        public void TempMaxHPDecrease(int value){
+            isTempHealth = false;
+            TemporaryHealth = 0;
+        }
+
+        public void FlatHeal (int flatheal)
+        {
+            if (CurrentHealth + flatheal > MaxHealth){
+                flatheal = MaxHealth - CurrentHealth; 
+            }
+            DamagePopUp indicator = Instantiate(HealthText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+            indicator.SetDamageText(flatheal.ToString());
+            CurrentHealth += flatheal;   
+        }
+
+
+
+        public void HealOverTimeX(int healAmount, float duration, float tickSpeed)
+        {
+            StartCoroutine(HealOverTimeCoroutine(healAmount, duration, tickSpeed));
+        }
+
+        private IEnumerator HealOverTimeCoroutine(int healAmount, float duration, float tickSpeed)
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                yield return new WaitForSeconds(tickSpeed);
+
+                // Apply healing
+                FlatHeal(healAmount);
+
+                // Update elapsed time
+                elapsedTime += tickSpeed;
+            }
+        }
+
+
+
+        private int initialdamage;
+        private double takendamageboost;
+        private bool isTakenboosted = false;
+
+        public void DamageBoost (double BoostValue)
+        {
+            initialdamage = damage;
+            damage = (int) (damage * BoostValue);
+        }
+
+        public void DamageBoostEnd (double BoostValue)
+        {
+            damage = initialdamage; 
+
+        }
+
+        public void TakenDamageBoost (double BoostValue)
+        {
+            takendamageboost = BoostValue;
+            isTakenboosted = true;
+        }
+
+        public void TakenDamageBoostEnd (double BoostValue)
+        {
+            takendamageboost = 1;
+            isTakenboosted = false;
+        }
+
+        public void AddEvasion(int evasionvalue){
+            evasion += evasionvalue;
+        }
+
+        public void RemoveEvasion(int evasionvalue){
+            evasion -= evasionvalue;
+        }
+
+        
+
+        public void IncreaseMovementSpeed(float walkS, float runS){
+            MovementScript.walkSpeed = walkS;
+            MovementScript.runSpeed = runS;
+        }
+
+        public void ResetMovementSpeed(){
+            MovementScript.walkSpeed = normalwalkSpeed;
+            MovementScript.runSpeed = normalrunSpeed;
+        }
+
+
+        private bool isburning = false;
+        private int activeDOTEffects = 0;
+
+        public IEnumerator TakeDamageOverTimeX(string DOTType, int damages, float duration, float tickSpeed)
+        {
+            float elapsedTime = 0f;
+
+            activeDOTEffects++;
+
+            while (elapsedTime < duration)
+            {
+                yield return new WaitForSeconds(tickSpeed);
+
+                // Apply damage
+                TakeDamage(damages);
+
+                // Update elapsed time
+                elapsedTime += tickSpeed;
+            }
+
+            // Decrement active DOT effects
+            activeDOTEffects--;
+
+        }
+
+        bool takenodamage = false;
+        public void GainImmunity (){
+            takenodamage = true;
+        }
+
+        public void RemoveImmunity (){
+            takenodamage = false;
+        }
+
+
+        public void TakeDamage (int damagetaken)
+        {
+            if (isTakenboosted == true){
+                damagetaken = (int) (damagetaken * takendamageboost);
+            }
+
+            if (Random.Range(0, 100) < evasion)
+            {
+                // Attack missed due to evasion
+                DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                indicator.SetDamageText("Miss");
+                indicator.SetDamageTextColor(Color.gray);
+                Debug.Log(transform.name + " evaded the attack!");
+            }
+            else
+            {
+                if(takenodamage == true){
+                    DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                    indicator.SetDamageText("Immune");
+                    indicator.SetDamageTextColor(Color.white);
+                }
+                else{
+                    damagetaken -= armor;
+                    damagetaken = Mathf.Clamp(damagetaken, 0, int.MaxValue);
+
+                    if(isTempHealth == true){
+                        if(damagetaken <= TemporaryHealth){
+                            TemporaryHealth -= damagetaken;
+                            DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                            indicator.SetDamageText(damagetaken.ToString());
+                            indicator.SetDamageTextColor(Color.white);
+                        }
+                        else if(damagetaken > TemporaryHealth){
+                            damagetaken -= TemporaryHealth;
+                            CurrentHealth -= damagetaken;
+                            isTempHealth = false;
+                            skills.IceP2.SetActive(false);
+                            DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                            indicator.SetDamageText(damagetaken.ToString());
+                            indicator.SetDamageTextColor(Color.white);
+                        }
+                    }
+                    else{
+                        CurrentHealth -= damagetaken;
+                        DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                        indicator.SetDamageText(damagetaken.ToString());
+                        indicator.SetDamageTextColor(Color.red);
+                    }
+                }
+            }
+        }
+
+
+        public void UseMana (int manacost)
+        {
+            if(CurrentMana < manacost){
+                DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+                indicator.SetDamageText("Need Mana");
+                indicator.SetDamageTextColor(Color.cyan);
+            }
+            else{
+                CurrentMana -= manacost;
+            }
+        }
+
+        public void NeedMana(){
+            DamagePopUp indicator = Instantiate(DamageText, transform.position, Quaternion.identity).GetComponent<DamagePopUp>();
+            indicator.SetDamageText("Need Mana");
+            indicator.SetDamageTextColor(Color.cyan);
+        }
+
+        public virtual void Die ()
+        {
+            Debug.Log(transform.name + " died.");
+        }
+}
