0b11adae5ba0e326bac16203ac791d706919b103
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-11-20 20:30:46 -0500

    Add files via upload

--- Lost in the Booty/Assets/UI/Scripts/AddSkillToSlot.cs
+++ Lost in the Booty/Assets/UI/Scripts/AddSkillToSlot.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AddSkillToSlot : MonoBehaviour
 {
 
     public Image original;
     public Sprite NewSprite;
-
+    public Image Skillbackground;
+    public TextMeshProUGUI warningtext;
 
     // Update is called once per frame
     void Update()
@@ -18,11 +20,17 @@ public class AddSkillToSlot : MonoBehaviour
 
     public void NewImage()
     {
-        original.sprite = NewSprite;
-        Color currentColor = original.color;
-        currentColor.a = 1f;
-        original.color = currentColor;
-
+        warningtext.gameObject.SetActive(false);
+        if(Skillbackground.gameObject.activeSelf == true){
+            warningtext.gameObject.SetActive(true);
+        }
+        else{
+            warningtext.gameObject.SetActive(false);
+            original.sprite = NewSprite;
+            Color currentColor = original.color;
+            currentColor.a = 1f;
+            original.color = currentColor;
+        }
     }
 
 }
