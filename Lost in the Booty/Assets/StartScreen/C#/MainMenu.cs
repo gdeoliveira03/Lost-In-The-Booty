ec4be7e6ae367b589bdb735a5d781bb693a9a3c2
Jordan Miller <123897858+JMiller007@users.noreply.github.com>
2023-11-24 16:09:36 -0500

    Fixing some things

--- Lost in the Booty/Assets/StartScreen/C#/MainMenu.cs
+++ Lost in the Booty/Assets/StartScreen/C#/MainMenu.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Start()
+    public void StartButton()
     {
+        SceneManager.LoadScene(0);
         Debug.Log("Start Game!");
     }
 
