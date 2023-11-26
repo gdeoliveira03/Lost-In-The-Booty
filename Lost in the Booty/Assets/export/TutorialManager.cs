bafc311c99cacd6c7309db9a445313bb6791c886
Jordan Miller <123897858+JMiller007@users.noreply.github.com>
2023-11-26 18:29:09 -0500

    Revert "TutorialFixes"
    This reverts commit 3e14fb6de111a5697669c9ecb78c80f0f436134f.

--- Lost in the Booty/Assets/export/TutorialManager.cs
+++ Lost in the Booty/Assets/export/TutorialManager.cs
@@ -9,93 +9,72 @@ public class TutorialManager : MonoBehaviour
     public TextMeshProUGUI tutorialText;
     private bool tutorialStarted = false;
 
-    private enum TutorialStep
-    {
-        Move,
-        RotateCamera,
-        Jump,
-        Attack,
-        Magic,
-        SelectSpell,
-        UseSpell,
-        ExploreIsland
-    }
-
-    private TutorialStep currentStep = TutorialStep.Move;
-
     private void Update()
     {
         if (!tutorialStarted)
         {
-            StartTutorial();
+            // Start the tutorial
+            StartCoroutine(ShowTutorial("Use WASD keys to move the character.", 8f));
             tutorialStarted = true;
         }
-
-        // Check for user input to advance tutorial
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            AdvanceTutorial();
-        }
-    }
-
-    private void StartTutorial()
-    {
-        StartCoroutine(ShowTutorial("Use WASD keys to move the character. Press 'F' for the next tutorial.", 500f));
+        // Add additional conditions to progress to the next steps if needed
     }
 
     IEnumerator ShowTutorial(string message, float duration)
     {
+        // Display the tutorial message
         DisplayTutorial(message);
 
         // Wait for the specified duration
         yield return new WaitForSeconds(duration);
 
-        if (currentStep != TutorialStep.ExploreIsland)
+        // Clear the tutorial text after the duration
+        tutorialText.text = "";
+
+        // Move to the next tutorial step after a delay
+        yield return new WaitForSeconds(1f); // Adjust the delay as needed
+
+        if (message.Contains("WASD keys"))
         {
-            tutorialText.text = "Press 'F' for the next tutorial.";
+            StartCoroutine(ShowTutorial("Use Q and E to rotate the camera.", 8f));
+            yield return new WaitForSeconds(5f); // Add a delay before the next message
+            StartCoroutine(ShowTutorial("Use V to change the camera angle.", 8f));
+        }
+        else if (message.Contains("Use Q and E to rotate the camera"))
+        {
+            StartCoroutine(ShowTutorial("Press Space to jump.", 8f));
+        }
+        else if (message.Contains("Press Space to jump"))
+        {
+            StartCoroutine(ShowTutorial("Use Left Mouse Button to attack with your weapon.", 8f));
+        }
+        else if (message.Contains("Left Mouse Button to attack with your weapon"))
+        {
+            StartCoroutine(ShowTutorial("Use Right Mouse Button to attack with magic.", 8f));
+        }
+        else if (message.Contains("Right Mouse Button to attack with magic"))
+        {
+            StartCoroutine(ShowTutorial("Click on the spell book to select spells.", 8f));
+            yield return new WaitForSeconds(8f); // Add a delay before the next message
+            StartCoroutine(ShowTutorial("Assign a spell to your hotkey by selecting the spell and setting it.", 8f));
+        }
+        else if (message.Contains("Click on the spell book to select spells"))
+        {
+            StartCoroutine(ShowTutorial("To use a spell, press the number on your keyboard that corresponds to the spell's hotkey number.", 8f));
+            //yield return new WaitForSeconds(5f); // Add a delay before the next message
+            //StartCoroutine(ShowTutorial("Assign a spell to your hotkey by selecting the spell and setting it.", 8f));
+
+        }
+        // Add more conditions for additional steps if needed
+        else if (message.Contains("To use a spell"))
+        {
+            StartCoroutine(ShowTutorial("Explore the island and find out what happened to you", 250f));
         }
     }
 
     void DisplayTutorial(string message)
     {
+        // Display the tutorial message
         tutorialText.text = message;
-    }
-
-    void AdvanceTutorial()
-    {
-        switch (currentStep)
-        {
-            case TutorialStep.Move:
-                currentStep = TutorialStep.RotateCamera;
-                StartCoroutine(ShowTutorial("Use Q and E to rotate the camera. Press 'F' for the next tutorial.", 500f));
-                break;
-            case TutorialStep.RotateCamera:
-                currentStep = TutorialStep.Jump;
-                StartCoroutine(ShowTutorial("Press Space to jump. Press 'F' for the next tutorial.", 500f));
-                break;
-            // Add more cases as needed
-            case TutorialStep.Jump:
-                currentStep = TutorialStep.Attack;
-                StartCoroutine(ShowTutorial("Use Left Mouse Button to attack with your weapon. Press 'F' for the next tutorial.", 500f));
-                break;
-            case TutorialStep.Attack:
-                currentStep = TutorialStep.Magic;
-                StartCoroutine(ShowTutorial("Use Right Mouse Button to attack with magic. Press 'F' for the next tutorial.", 500f));
-                break;
-            case TutorialStep.Magic:
-                currentStep = TutorialStep.SelectSpell;
-                StartCoroutine(ShowTutorial("Click on the spell book to select spells. Press 'F' for the next tutorial.", 500f));
-                break;
-            case TutorialStep.SelectSpell:
-                currentStep = TutorialStep.UseSpell;
-                StartCoroutine(ShowTutorial("To use a spell, press the number on your keyboard that corresponds to the spell's hotkey number. Press '' for the next tutorial.", 500f));
-                break;
-            case TutorialStep.UseSpell:
-                currentStep = TutorialStep.ExploreIsland;
-                StartCoroutine(ShowTutorial("Explore the island and find out what happened to you", 500f));
-                break;
-            default:
-                break;
-        }
     }
 }
