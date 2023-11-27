2b5659778239898b4859343b34933ede09e2235f
fundyp <144487692+fundyp@users.noreply.github.com>
2023-11-27 12:35:31 -0500

    sounds and chat

--- Lost in the Booty/Assets/SoundsAndMusic/footsteps.cs
+++ Lost in the Booty/Assets/SoundsAndMusic/footsteps.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class footsteps : MonoBehaviour
+
 {
-    public AudioSource footstepsSound, sprintSound;
+    private bool isRunning;
+    public AudioSource footstepsSound, sprintSound, autoSound, magicSound;
 
     void Update()
     {
+
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            if(autoSound.enabled == false) 
+            {
+                Invoke("handleAutoAudioOn", .5f);
+            }
+                
+        }
+
+
+        if(Input.GetMouseButtonDown(1)) 
+        {
+            if(magicSound.enabled == false) 
+            {
+                handleMagicAudioOn();
+            }
+                
+        }
+            
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
+            
+
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 footstepsSound.enabled = false;
@@ -26,6 +51,28 @@ public class footsteps : MonoBehaviour
             footstepsSound.enabled = false;
             sprintSound.enabled = false;
         }
+    }
+
+    void handleAutoAudioOn()
+    {
+        autoSound.enabled = true;
+        Invoke("handleAutoAudioOff", 1f);
+    }
+
+    void handleAutoAudioOff()
+    {
+        autoSound.enabled = false;
+    }
+
+    void handleMagicAudioOn()
+    {
+        magicSound.enabled = true;
+        Invoke("handleMagicAudioOff", 1f);
+    }
+
+    void handleMagicAudioOff()
+    {
+        magicSound.enabled = false;
     }
 }
 
